c16f90861c9ef52f06d7e93c0a1718950e03cd9f
Malek-Attia08 <malekattia8004@gmail.com>
2026-01-01 19:04:49 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,18 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+    <BrowserRouter>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/patient" element={<PatientPortal />} />
           <Route path="/doctor" element={<DoctorDashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+      </TooltipProvider>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
